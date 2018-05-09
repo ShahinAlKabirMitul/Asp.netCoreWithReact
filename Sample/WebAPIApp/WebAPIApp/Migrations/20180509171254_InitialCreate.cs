fd899bc586557faacea8390d66efcaa37b8f6625
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-05-09 23:15:11 +0600

    Configure Entityfreamwork core 2 & migrations added

---
+++ Sample/WebAPIApp/WebAPIApp/Migrations/20180509171254_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebAPIApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_Name",
+                table: "Students",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_Phone",
+                table: "Students",
+                column: "Phone",
+                unique: true,
+                filter: "[Phone] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
