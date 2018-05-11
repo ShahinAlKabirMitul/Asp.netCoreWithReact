e84316ad92889a4a3741e01c092cfe73d41af53c
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-05-11 17:29:02 +0600

    Database creation OK

---
+++ lms/LmsApp/Model/LmsDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model
+{
+   public class LmsDbContext : DbContext
+    {
+        public LmsDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Course>().HasIndex(s => s.Name);
+            modelBuilder.Entity<Course>().HasIndex(s => s.Topic);
+            modelBuilder.Entity<Course>().HasIndex(s => s.IsFree);
+
+
+        }
+    }
+}
