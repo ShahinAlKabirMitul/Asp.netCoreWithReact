a3ca3305550a20c3855c3ab87a125ea98f6826e8
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-05-12 23:31:49 +0600

    Startup coded

--- lms/LmsApp/LmsApp.Server/Startup.cs
+++ lms/LmsApp/LmsApp.Server/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Model;
+using Model.Repo;
+using Service;
 
 namespace LmsApp.Server
 {
@@ -27,6 +29,8 @@ namespace LmsApp.Server
         {
             services.AddDbContext<LmsDbContext>(options=>
                 options.UseSqlServer(Configuration.GetConnectionString("LmsDbContext")) );
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped(typeof(IGenericService<,,>), typeof(GenericService<,,>));
             services.AddMvc();
         }
 
