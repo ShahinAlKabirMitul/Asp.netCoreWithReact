51ec1faa7be4546467060579d421faa884b16605
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-05-18 11:52:21 +0600

    Studnent filter and clickable complete

--- Sample/my-app/src/App.js
+++ Sample/my-app/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HttpRepo from './Repository/repository';
+import Greeting from './GreetingsDiv';
 import Student from './student';
+import StudentFilter from './studentFilter';
+import HttpRepo from './Repository/repository';
 
 class App extends Component {
   constructor() {
@@ -13,7 +15,22 @@ class App extends Component {
       students: [],
       filteredStudents: [],
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleStudentFilter = this.handleStudentFilter.bind(this);
   }
+  handleClick(x) {
+    this.setState({ student: x });
+    console.log(this.state);
+  }
+
+  handleStudentFilter(event) {
+    this.setState({ keyword: event.target.value });
+    var result = this.state.students.filter(
+      x => x.name.indexOf(event.target.value) !== -1
+    );
+    this.setState({ filteredStudents: result });
+  }
+
   componentWillMount() {
     let r = new HttpRepo();
 
@@ -30,13 +47,31 @@ class App extends Component {
     let values = ['first', 'second', 'third'];
 
     return (
-      <ul>
-        {this.state.filteredStudents.map(x => (
-          <li key={x.id}>
-            <Student studentObj={x} />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <StudentFilter handleFilter={this.handleStudentFilter} />
+        <h2>Filter : {this.state.keyword}</h2>
+        <ul>
+          {this.state.filteredStudents.map(x => (
+            <li key={x.id}>
+              <Student studentObj={x} handleClick={this.handleClick} />
+            </li>
+          ))}
+        </ul>
+        <div>
+          <h3>Click on the name of the above students: </h3>
+          <Greeting student={this.state.student} />
+        </div>
+        <div>
+          <p>
+            <a
+              href="https://github.com/ShahinAlKabirMitul/Asp.netCoreWithReact/tree/master/Sample/my-app"
+              target="_blank"
+            >
+              https://github.com/ShahinAlKabirMitul/Asp.netCoreWithReact/tree/master/Sample/my-app
+            </a>
+          </p>
+        </div>
+      </div>
     );
   }
 }
